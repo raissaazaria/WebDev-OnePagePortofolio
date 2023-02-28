2bfa201b3db7c873d097d7d1b86155a496c01742
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2023-02-28 10:53:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Freelancer', 'Web Developer', 'Graphic Designer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function typing(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.typing-effect').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(type, 500);
+
+})();
